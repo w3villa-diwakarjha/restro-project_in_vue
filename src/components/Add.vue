b19f278d181ab170af2dafb7b7e3ab857b521000
Diwakar Jha <diwakar.jha@w3villa.com>
2023-08-22 18:30:18 +0530

    Missing Placeholder Issue is Fix

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -2,7 +2,7 @@
     <Header/>
     <h1>Welcome to Add Section</h1>
     <form action="" class="add-resturant">
-        <input type="text" placehol~der="Enter Name" v-model="resturant.name" name="name"/>
+        <input type="text" placeholder="Enter Name" v-model="resturant.name" name="name"/>
         <input type="text" placeholder="Enter Address" v-model="resturant.Address" name="Address"/>
         <input type="text" placeholder="Enter Contact" v-model="resturant.contact" name="Contact"/>
         <button type="button" v-on:click="addresturant">Add New Resturant</button>
